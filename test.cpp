24a7e873d25315e3ba317bcf9b0099066156028a
firewave <firewave@users.noreply.github.com>
2025-11-05 15:45:18 +0100

    fixed #578 - removed support for `#file` and `#endfile` [skip ci]

--- test.cpp
+++ test.cpp
@@ -2453,8 +2453,6 @@ static void location11()
                   preprocess(code));
 }
 
-// TODO: test #file/#endfile
-
 static void missingHeader1()
 {
     const char code[] = "#include \"notexist.h\"\n";
